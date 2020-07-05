7c8f4d0d58e8c149246a18228d046230b842ab5f
Jared Counts <countsjared@gmail.com>
2020-07-05 15:31:38 -0700

    Clean up the bresenham implementation

--- src/bresenham.ts
+++ src/bresenham.ts
@@ -1,55 +1,39 @@
-
 type bresenhamCallbackType = (x: number, y: number) => void;
 
+/**
+ * Run bresenham's line algorithm from the start position to the end position,
+ * and perform the given callback function on each integer point between those
+ * two positions. 
+ * Based on http://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+ */
 export function Bresenham(
     startX : number, startY : number, 
     endX : number, endY : number,
     callback : bresenhamCallbackType) : void 
 {
-    var dx = Math.abs(endX - startX);
-    var dy = Math.abs(endY - startY);
+    let dx = Math.abs(endX - startX);
+    let dy = Math.abs(endY - startY);
 
-    var sx;
-    var sy;
-    if (startX < endX)
-        sx = 1;
-    else
-        sx = -1;
-    if (startY < endY)
-        sy = 1;
-    else
-        sy = -1;
+    let sx = startX < endX ? 1 : -1;
+    let sy = startY < endY ? 1 : -1;
     
-    var err = dx - dy;
-    var x0 = startX;
-    var y0 = startY;
-    var x1 = endX;
-    var y1 = endY;
-    var i = 0;
+    let err = dx - dy;
+    let x = startX;
+    let y = startY;
 
-    while ((x0 != x1) || (y0 != y1)) {
+    while (x != endX || y != endY) {
+        callback(x, y);
 
-        callback(x0, y0);
-
-        // var cellI = 
-        //     THREE.MathUtils.clamp(x0, 
-        //         /* min */ 0, 
-        //         /* max */ waveSolver.GetCellCountX() - 1);
-        // var cellJ = 
-        //     THREE.MathUtils.clamp(y0,
-        //         /* min */ 0, 
-        //         /* max */ waveSolver.GetCellCountY() - 1);
+        let e2 = 2 * err;
         
-        // waveSolver.AddVelocity(force, cellI, cellJ);
-
-        var e2 = 2 * err;
         if (e2 > -dy) {
           err -= dy;
-          x0 = x0 + sx;
+          x = x + sx;
         }
+
         if (e2 < dx) {
           err = err + dx;
-          y0 = y0 + sy;
+          y = y + sy;
         }
     }
 }
