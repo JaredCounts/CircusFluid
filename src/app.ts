356c973929524f357ee6fa135b800a1c80ceed82
Jared Counts <countsjared@gmail.com>
2020-07-05 13:35:51 -0700

    More cleanup ... separated out view logic

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import * as THREE from 'three';
 import { WaveSolver } from './waveSolver'
 import { Controller } from './controller'
 import { TouchPos } from './touchPos'
-import { bresenham } from './bresenham'
+import { View } from './view'
 
 import * as Util from './util'
 
@@ -14,79 +14,88 @@ import * as Util from './util'
 // ====================
 
 // create the scene
-const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera(
-    /* left */ -50,
-    /* right */ 50,
-    /* top */ -50,
-    /* bottom */ 50,
-    /* near */ 0.1, 
-    /* far */ 7000);
+// const scene = new THREE.Scene();
+// const camera = new THREE.OrthographicCamera(
+//     /* left */ -50,
+//     /* right */ 50,
+//     /* top */ -50,
+//     /* bottom */ 50,
+//     /* near */ 0.1, 
+//     /* far */ 7000);
 
 // Renderer setup
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(view.GetDomElement());
+
 
 // Update the renderer and wave solver when the window resizes.
 function onWindowResize() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var cellCountX = Math.floor(window.innerWidth / cellWidth);
-    var cellCountY = Math.floor(window.innerHeight / cellWidth);
     waveSolver = new WaveSolver(cellCountX, cellCountY);
+    controller = new Controller(waveSolver);
+    registerControllerHandlers();
+
+    let oldDomElement = view.GetDomElement();
+    view = new View(window, waveSolver);
+    document.body.replaceChild(view.GetDomElement(), oldDomElement);
+    // view.GetRenderer().setSize(window.innerWidth, window.innerHeight);
+
+    // var cellCountX = Math.floor(window.innerWidth / cellWidth);
+    // var cellCountY = Math.floor(window.innerHeight / cellWidth);
+    // waveSolver = new WaveSolver(cellCountX, cellCountY);
 }
 
 var textureClock = new THREE.Clock(false);
 var textureTimes = [];
 
-function GetWaveTexture(waveSolver) : void {
-    textureClock.start();
+// function GetWaveTexture(waveSolver) : void {
+//     textureClock.start();
 
-    var r = 0;
-    var g = 0;
-    var b = 0;
+//     var r = 0;
+//     var g = 0;
+//     var b = 0;
 
-    var countX = waveSolver.GetCellCountX();
-    var countY = waveSolver.GetCellCountY();
+//     var countX = waveSolver.GetCellCountX();
+//     var countY = waveSolver.GetCellCountY();
 
-    var cellSizeX = textureWidth / countX;
-    var cellSizeY = textureHeight / countY;
+//     var cellSizeX = textureWidth / countX;
+//     var cellSizeY = textureHeight / countY;
 
-    for (let j = 0; j < textureHeight; j++) {
-        for (let i = 0; i < textureWidth; i++) {
-            let index = i + j * textureWidth;
+//     for (let j = 0; j < textureHeight; j++) {
+//         for (let i = 0; i < textureWidth; i++) {
+//             let index = i + j * textureWidth;
 
-            var x = i/textureWidth * 2 - 1;
-            var y = j/textureHeight * 2 - 1;
+//             var x = i/textureWidth * 2 - 1;
+//             var y = j/textureHeight * 2 - 1;
 
-            var pixel = new THREE.Vector2(x, y);
+//             var pixel = new THREE.Vector2(x, y);
 
-            var cellI = i / cellSizeX;
-            var cellJ = j / cellSizeY;
+//             var cellI = i / cellSizeX;
+//             var cellJ = j / cellSizeY;
 
-            var density = waveSolver.GetDensity(cellI, cellJ);
-            var velocity = waveSolver.GetVelocity(cellI, cellJ);
-            var color = new THREE.Color(); // 127 + 127 * Math.sin(velocity * 0.0004)
+//             var density = waveSolver.GetDensity(cellI, cellJ);
+//             var velocity = waveSolver.GetVelocity(cellI, cellJ);
+//             var color = new THREE.Color(); // 127 + 127 * Math.sin(velocity * 0.0004)
 
-            var hsl = Util.hsvToHsl(
-                0.5 + 0.5 * Math.sin(density*0.0004), 
-                1.0, 
-                0.5 + 0.5 * Math.sin(velocity*0.01));
+//             var hsl = Util.hsvToHsl(
+//                 0.5 + 0.5 * Math.sin(density*0.0004), 
+//                 1.0, 
+//                 0.5 + 0.5 * Math.sin(velocity*0.01));
 
-            color.setHSL(hsl[0], hsl[1], hsl[2]);
+//             color.setHSL(hsl[0], hsl[1], hsl[2]);
 
-            var stride = index * 3;
-            textureData[ stride ]     = Math.floor(color.r * 255);
-            textureData[ stride + 1 ] = Math.floor(color.g * 255);
-            textureData[ stride + 2 ] = Math.floor(color.b * 255);
-        }
-    }
+//             var stride = index * 3;
+//             textureData[ stride ]     = Math.floor(color.r * 255);
+//             textureData[ stride + 1 ] = Math.floor(color.g * 255);
+//             textureData[ stride + 2 ] = Math.floor(color.b * 255);
+//         }
+//     }
 
-    textureClock.stop();
+//     textureClock.stop();
 
-    textureTimes.push(textureClock.getElapsedTime());
-}
+//     textureTimes.push(textureClock.getElapsedTime());
+// }
 
 var cellWidth = 5;
 var cellCountX = Math.floor(window.innerWidth / cellWidth);
@@ -94,42 +103,8 @@ var cellCountY = Math.floor(window.innerHeight / cellWidth);
 
 var waveSolver = new WaveSolver(cellCountX, cellCountY);
 
-// // console.log(ws.getCellSize());
-// waveSolver.Solve(0.1);
-
-// var texture = GetWaveTexture(waveSolver);
-
-var textureWidth = waveSolver.GetCellCountX();
-var textureHeight = waveSolver.GetCellCountY();
-var textureSize = textureWidth * textureHeight;
-var textureData = new Uint8Array( 3 * textureSize );
-
-var material2 = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, map: texture});
-var texture = new THREE.DataTexture( textureData, textureWidth, textureHeight, THREE.RGBFormat );
-GetWaveTexture(waveSolver);
-
-material2.map = texture;
-
-var geometry2 = new THREE.PlaneGeometry(100, 100);
-var plane = new THREE.Mesh( geometry2, material2 );
-scene.add( plane );
-
-plane.position.x = 0;
-plane.position.y = 0;
-plane.position.z = 0;
-
-// plane.rotation.x = Math.PI/2;
-
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 100;
-
-camera.lookAt(scene.position);
-
-// var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
-// var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-// var plane = new THREE.Mesh( geometry, material );
-// scene.add( plane );
+var view = new View(window, waveSolver);
+document.body.appendChild(view.GetDomElement());
 
 function animate(): void {
     requestAnimationFrame(animate);
@@ -183,7 +158,6 @@ function render(): void {
         timestepCount = 10;
     }
 
-    // console.log(timestepCount)
     for (var i = 0; i < timestepCount; i++) {
         solveClock.start();
         waveSolver.Solve(timestepSec);
@@ -192,24 +166,27 @@ function render(): void {
         solveTimes.push(solveClock.getElapsedTime());
     }
 
-    GetWaveTexture(waveSolver);
-    material2.map.needsUpdate = true;
+    // // GetWaveTexture(waveSolver);
+    // material.map.needsUpdate = true;
+    view.Render();
 
+    // renderer.render(scene, camera);
+}
 
-    renderer.render(scene, camera);
+function registerControllerHandlers() {
+    window.addEventListener( 'touchmove', controller.HandleTouchMove.bind(controller), false );
+    window.addEventListener( 'touchstart', controller.HandleTouchStart.bind(controller), false );
+    window.addEventListener( 'touchend', controller.HandleTouchEnd.bind(controller), false );
+    window.addEventListener( 'touchleave', controller.HandleTouchEnd.bind(controller), false );
+
+    window.addEventListener( 'mousemove', controller.HandleMouseMove.bind(controller), false );
+    window.addEventListener( 'mousedown', controller.HandleMouseDown.bind(controller), false );
+    window.addEventListener( 'mouseup', controller.HandleMouseUp.bind(controller), false );
+    window.addEventListener( 'click', controller.HandleMouseClick.bind(controller), true);
 }
 
 var controller = new Controller(waveSolver);
-
-window.addEventListener( 'touchmove', controller.HandleTouchMove.bind(controller), false );
-window.addEventListener( 'touchstart', controller.HandleTouchStart.bind(controller), false );
-window.addEventListener( 'touchend', controller.HandleTouchEnd.bind(controller), false );
-window.addEventListener( 'touchleave', controller.HandleTouchEnd.bind(controller), false );
-
-window.addEventListener( 'mousemove', controller.HandleMouseMove.bind(controller), false );
-window.addEventListener( 'mousedown', controller.HandleMouseDown.bind(controller), false );
-window.addEventListener( 'mouseup', controller.HandleMouseUp.bind(controller), false );
-window.addEventListener( 'click', controller.HandleMouseClick.bind(controller), true);
+registerControllerHandlers();
 
 window.addEventListener( 'resize', onWindowResize, false );
 
---
+++ src/view.ts
@@ -0,0 +1,134 @@
+import * as THREE from 'three';
+
+import { WaveSolver } from './waveSolver'
+import * as Util from './util'
+
+export class View {
+    private readonly scene : THREE.Scene;
+    private readonly renderer : THREE.WebGLRenderer;
+    private readonly camera : THREE.Camera;
+
+    private readonly texture : THREE.DataTexture;
+    private readonly material : THREE.MeshBasicMaterial;
+    private textureData : Uint8Array;
+    
+    private readonly waveSolver : WaveSolver;
+
+    private readonly textureWidth : number;
+    private readonly textureHeight : number;
+    // Reference to the current window
+    // private readonly window;
+
+    constructor(window, waveSolver : WaveSolver) {
+        this.waveSolver = waveSolver;
+
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        
+        this.camera = new THREE.OrthographicCamera(
+            /* left */ -50,
+            /* right */ 50,
+            /* top */ -50,
+            /* bottom */ 50,
+            /* near */ 0.1,
+            /* far */ 7000);
+
+        // this.window = window;
+        this.textureWidth = this.waveSolver.GetCellCountX();
+        this.textureHeight = this.waveSolver.GetCellCountY();
+
+        let textureSize = this.textureWidth * this.textureHeight;
+        this.textureData = new Uint8Array(3 * textureSize);
+
+        this.texture = new THREE.DataTexture(
+            this.textureData, 
+            this.textureWidth, 
+            this.textureHeight, 
+            THREE.RGBFormat);
+
+        this.material = new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide, 
+            map: this.texture
+        });
+
+        this.material.map = this.texture;
+
+        let geometry = new THREE.PlaneGeometry(100, 100);
+        let plane = new THREE.Mesh(geometry, this.material);
+        this.scene.add( plane );
+
+        this.camera.position.x = 0;
+        this.camera.position.y = 0;
+        this.camera.position.z = 100;
+
+        this.camera.lookAt(this.scene.position);
+    }
+
+    private _Update() : void {
+        // Update the data texture to reflect the wave solver state.
+
+        let countX = this.waveSolver.GetCellCountX();
+        let countY = this.waveSolver.GetCellCountY();
+
+        let cellSizeX = this.textureWidth / countX;
+        let cellSizeY = this.textureHeight / countY;
+
+        for (let j = 0; j < this.textureHeight; j++) {
+            for (let i = 0; i < this.textureWidth; i++) {
+                let index = i + j * this.textureWidth;
+
+                // Calculate the data texture pixel position
+                let x = i / this.textureWidth * 2 - 1;
+                let y = j / this.textureHeight * 2 - 1;
+
+                let pixel = new THREE.Vector2(x, y);
+
+                // Calculate the wave solver cell position
+                let cellI = i / cellSizeX;
+                let cellJ = j / cellSizeY;
+
+                let density = this.waveSolver.GetDensity(cellI, cellJ);
+                let velocity = this.waveSolver.GetVelocity(cellI, cellJ);
+
+                // Compute the color from HSV space
+                let hsl = Util.hsvToHsl(
+                    0.5 + 0.5 * Math.sin(density*0.0004), 
+                    1.0, 
+                    0.5 + 0.5 * Math.sin(velocity*0.01));
+
+                let color = new THREE.Color();
+                color.setHSL(hsl[0], hsl[1], hsl[2]);
+
+                let stride = index * 3;
+                this.textureData[stride]     = Math.floor(color.r * 255);
+                this.textureData[stride + 1] = Math.floor(color.g * 255);
+                this.textureData[stride + 2] = Math.floor(color.b * 255);
+            }
+        }
+
+        // Signal the material to update its texture
+        this.material.map.needsUpdate = true;
+    }
+
+    Render() : void {
+        this._Update();
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    GetScene() : THREE.Scene {
+        return this.scene;
+    }
+
+    GetCamera() : THREE.Camera {
+        return this.camera;
+    }
+
+    GetRenderer() : THREE.WebGLRenderer {
+        return this.renderer;
+    }
+
+    GetDomElement() {
+        return this.renderer.domElement;
+    }
+}
